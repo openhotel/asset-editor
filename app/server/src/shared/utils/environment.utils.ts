e60f0d0ade54952480c2c120530b6b19261451f5
pagoru <pagoru@gmail.com>
2024-08-20 14:47:22 +0200

    feat: Added open browser on production - fix #1

--- app/server/src/shared/utils/environment.utils.ts
+++ app/server/src/shared/utils/environment.utils.ts
@@ -1,6 +1,7 @@
 import { Envs } from "shared/types/main.ts";
+import { System } from "system/main.ts";
 
-export const isDevelopment = () => Deno.env.get("ENV") === "development";
+export const isDevelopment = () => System.getEnvs().isDevelopment;
 
 export const getProcessedEnvs = ({ version }: Envs): Envs => ({
   version: version === "__VERSION__" ? "DEVELOPMENT" : version,
--- app/server/src/system/main.ts
+++ app/server/src/system/main.ts
@@ -2,25 +2,33 @@ import { api } from "./api.ts";
 import { ConfigTypes, Envs } from "shared/types/main.ts";
 import { getConfig as $getConfig } from "shared/utils/main.ts";
 import { load as loadUpdater } from "../modules/updater/main.ts";
+import { open } from "open";
 
 export const System = (() => {
   const $api = api();
 
   let $config: ConfigTypes;
+  let $envs: Envs;
 
   const load = async (envs: Envs) => {
+    $envs = envs;
+
     if (await loadUpdater(envs)) return;
 
     $config = await $getConfig();
 
     $api.load();
+
+    if (!$envs.isDevelopment) await open($config.url, { wait: true });
   };
 
   const getConfig = (): ConfigTypes => $config;
+  const getEnvs = (): Envs => $envs;
 
   return {
     load,
     getConfig,
+    getEnvs,
 
     api: $api,
   };
