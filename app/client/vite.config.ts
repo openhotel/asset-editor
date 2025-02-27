0b940319a8c0e82f37147fdce38bbef2d4a738f2
Diego González Liarte <diegogliarte@gmail.com>
2025-02-27 20:34:33 +0100

    add PROXY_URL for windows

--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -3,11 +3,16 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
+const PROXY_URL =
+  process.platform === "win32"
+    ? "http://127.0.0.1:20300"
+    : "http://localhost:20300";
+
 export default defineConfig({
   server: {
     port: 2030,
     proxy: {
-      "/api": "http://localhost:20300",
+      "/api": PROXY_URL,
     },
   },
   plugins: [react(), reactRefresh(), viteSingleFile()],
